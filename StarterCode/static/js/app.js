bad2e6ae35d8fe3e3a67fae6e7697e034acd677e
Sundaramurthy Jagadeesan <revathi22@gmail.com>
2021-07-29 23:57:09 -0500

    hi made bar chart

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -9,7 +9,31 @@ function Plots(sampleId){
         console.log(data);
     var Filtereddata = data.samples.filter(g => g.id == sampleId);
     console.log(Filtereddata);
+    var sample_values =Filtereddata[0].sample_values;
+    console.log(sample_values);
+    var otu_ids = Filtereddata[0].otu_ids;
+    console.log(otu_ids)
+    var otu_id_label= otu_ids.map(x=>"OTU" +x);
+    var wFreq = data.metadata.Filter(g => g.id ==sampleId)
+        
+    }
     })
+    var tracebar ={
+        x: Sample_values.slice(0,10).reverse(),
+        y: otu_id_label.slice(0,10).reverse(),
+        text:otu_labels
+        type:"bar"
+        orientation: 'h'
+
+
+    var dataBar =[traceBar];
+    var layout Bar ={
+        height : 500,
+        width: 500
+
+    }
+    Plotly.newPlot('bar',dataBar,layout Bar);
+    };
 
 
 
